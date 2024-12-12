9e71504cd2b14dd3e9f66cd9647558148069ba4e
kosche1 <zedrickpiolocastro@gmail.com>
2024-12-12 11:46:44 +0000

    Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://kosche1.github.io',
+	base: '/Castro-Docs',
 	integrations: [
 		starlight({
 			title: 'My Docs',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/withastro/starlight',	
 			},
 			sidebar: [
 				{
